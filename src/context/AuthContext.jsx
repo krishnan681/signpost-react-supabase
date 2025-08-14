3f3553da86760d44ffa95e47dc98f2d633498bf7
krishnan681 <gopalakrishnan0614@gmail.com>
2025-08-14 15:29:54 +0530

    changed src file for authcontext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../services/supabaseClient";
+import { supabase } from "../../src/services/supabaseClient";
 import Swal from "sweetalert2";
 
 const AuthContext = createContext();
